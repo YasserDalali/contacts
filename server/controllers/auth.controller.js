6af3aaacc7a40da22a058ac6b4200aae28c33162
YasserDalali <yasser.dalali.personal@gmail.com>
2025-04-22 14:08:49 +0200

    feat: reverted to old auth controller setup

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,68 +1,90 @@
-import mongoose from "mongoose";
-import User from "../models/User.model.js";
-import generateToken from "../utils/jwtsign.js";
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose';
+import User from '../models/User.model.js';
+import generateToken from "../utils/jwtsign.js"
+
 
 class AuthController {
-  static async register(req, res) {
-    const session = await mongoose.startSession();
-    try {
-      session.startTransaction();
-      const { username, email, password } = req.body;
+    static async register(req, res) {
+        const session = await mongoose.startSession();
+        try {
+            session.startTransaction();
+            const { username, email, password } = req.body;
+            
+            if (!(username && email && password)) {
+                throw new Error("Fill all fields (username, email, password)");
+            }
 
-      UserService.validateUser({ username, email, password });
-      const hashedPassword = UserService.hashPassword(password);
+            const existingEmail = await User.findOne({ email: email });
+            const existingUsername = await User.findOne({ username: username });
+            
+            if (existingEmail || existingUsername) {
+                throw new Error("Username/Email already taken");
+            }
 
-      const newUser = await User.create(
-        [
-          {
-            username,
-            email,
-            password: hashedPassword,
-            joining_date: new Date(),
-          },
-        ],
-        { session }
-      );
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            
+            const newUser = await User.create([{
+                username,
+                email,
+                password: hashedPassword,
+                joining_date: new Date()
+            }], { session });
 
-      const token = generateToken(newUser[0]);
+            const token = generateToken(newUser[0]);
+            
+            await session.commitTransaction();
+            res.status(201).json({
+                message: "Success",
+                data: {
+                    user: newUser[0],
+                    token: token
+                }
+            });
+        } catch (error) {
+            await session.abortTransaction();
+            res.status(400).json({
+                message: error.message || "Registration failed"
+            });
+        } finally {
+            await session.endSession();
+        }
+    }   
 
-      await session.commitTransaction();
-      res.status(201).json({
-        message: "Success",
-        data: {
-          user: newUser[0],
-          token: token,
-        },
-      });
-    } catch (error) {
-      await session.abortTransaction();
-      res.status(400).json({
-        message: error.message || "Registration failed",
-      });
-    } finally {
-      await session.endSession();
+    static async login(req, res) {
+        try {
+            const { email, password } = req.body;
+            
+            if (!(email && password)) {
+                throw new Error("Fill all fields (email, password)");
+            }
+            
+            const user = await User.findOne({ email: email });
+            if (!user) {
+                throw new Error("No user found");
+            }
+
+            const passValid = await bcrypt.compare(password, user.password);
+            if (!passValid) {
+                throw new Error("Invalid password");
+            }
+
+            const token = generateToken(user);
+            res.status(200).json({
+                message: "Login successful",
+                data: {
+                    user: user,
+                    token: token
+                }
+            });
+            
+        } catch (error) {
+            res.status(401).json({
+                message: error.message || "Login failed"
+            });
+        }
     }
-  }
-
-  static async login(req, res) {
-    try {
-      const { email, password } = req.body;
-      const user = UserService.findUserByEmail(email, password);
-
-      const token = generateToken(user);
-      res.status(200).json({
-        message: "Login successful",
-        data: {
-          user: user,
-          token: token,
-        },
-      });
-    } catch (error) {
-      res.status(401).json({
-        message: error.message || "Login failed",
-      });
-    }
-  }
 }
 
-export default AuthController;
+export default AuthController
