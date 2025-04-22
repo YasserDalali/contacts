568f075ad258df9930c70fcc70fc247b96e7cb7a
YasserDalali <yasser.dalali.personal@gmail.com>
2025-04-22 13:37:32 +0200

    feat: add Gemini service integration and update environment configuration

--- server/config/config.js
+++ server/config/config.js
@@ -3,4 +3,4 @@ import process from "process";
 
 config({ path: '.env' });
 
-export const { PORT, MONGO_URI, JWT_EXPIRE, JWT_SECRET, ARCJET_KEY, ARCJET_ENV } = process.env;
+export const { PORT, GEMINI_API_KEY, MONGO_URI, JWT_EXPIRE, JWT_SECRET, ARCJET_KEY, ARCJET_ENV } = process.env;
---
+++ server/services/Gemini.service.js
@@ -0,0 +1,15 @@
+import { GEMINI_API_KEY } from "../config.js";
+import { GoogleGenAI } from "@google/genai";
+
+ async function execPrompt(prompt, data, reSchema) {
+        const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+                  const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `{prompt: ${prompt}, data: ${data}}`,
+          });
+          return response.text;
+    }
+
+
+
+export default execPrompt;
